67157514ec8cbe1cb7099b541580fe7d52b088a2
quentin basset <quentin.basset@querio.com>
2021-06-17 22:32:56 +0200

    :sparkles: integrate login component

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,47 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+import { TokenService } from '../services/token.service';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
+  isLoginInvalid = false;
+  loginForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required]),
+  });
 
-  constructor() { }
+  constructor(
+    private authService: AuthService,
+    private tokenService: TokenService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
 
-  ngOnInit(): void {
+  redirectoToHome() {
+    this.router.navigate(['/home'], {
+      relativeTo: this.route,
+    });
   }
 
+  ngOnInit(): void {
+    if (this.tokenService.isLogged) {
+      this.redirectoToHome();
+    }
+  }
+
+  onSubmit() {
+    if (!this.loginForm.valid) {
+      return;
+    }
+    this.authService.login(this.loginForm.value).subscribe(
+      () => this.redirectoToHome(),
+      (error) => (this.isLoginInvalid = true)
+    );
+  }
 }
