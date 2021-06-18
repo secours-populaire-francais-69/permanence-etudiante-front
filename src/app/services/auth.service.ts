db71144a51d4183bca29de7455e2d3b07ee73c82
quentin basset <quentin.basset@querio.com>
2021-06-18 22:35:22 +0200

    :sparkles: add forgottenPassword and resetPassword to auth service

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -4,12 +4,18 @@ import { environment } from '../../environments/environment';
 import { Login, LoginResponse } from '../interfaces/login';
 import { TokenService } from './token.service';
 import { tap } from 'rxjs/operators';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  constructor(private http: HttpClient, private tokenService: TokenService) {}
+  constructor(
+    private http: HttpClient,
+    private tokenService: TokenService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
 
   login(loginValues: Login) {
     return this.http
@@ -24,5 +30,35 @@ export class AuthService {
   logout() {
     this.tokenService.token = null;
     this.tokenService.redirectoToLogin();
+  }
+
+  forgottenPassword(forgottenPassword: { email: string }) {
+    return this.http.post<{ status: string }>(
+      `${environment.api}forgotten-password`,
+      {
+        forgottenPassword,
+      }
+    );
+  }
+
+  resetPassword(resetPassword: {
+    resetPasswordToken: string;
+    password: string;
+  }) {
+    return this.http
+      .post<LoginResponse>(`${environment.api}reset-password`, {
+        resetPassword,
+      })
+      .pipe(
+        tap((response) => {
+          this.tokenService.token = response.data.token;
+        })
+      );
+  }
+
+  redirectoToHome() {
+    this.router.navigate(['/home'], {
+      relativeTo: this.route,
+    });
   }
 }
