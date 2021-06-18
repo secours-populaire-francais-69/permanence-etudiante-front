fa83a1d8d8f34fd8c0ec8575c10b291b39f11f9c
quentin basset <quentin.basset@querio.com>
2021-06-18 22:36:19 +0200

    :sparkles: add forgotten password component

---
+++ src/app/forgotten-password/forgotten-password.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../services/auth.service';
+import { TokenService } from '../services/token.service';
+
+@Component({
+  selector: 'app-forgotten-password',
+  templateUrl: './forgotten-password.component.html',
+  styleUrls: ['./forgotten-password.component.scss'],
+})
+export class ForgottenPasswordComponent implements OnInit {
+  isEmailSent = false;
+  forgottenPasswordForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+  });
+
+  constructor(
+    private authService: AuthService,
+    private tokenService: TokenService
+  ) {}
+
+  ngOnInit(): void {
+    if (this.tokenService.isLogged) {
+      this.authService.redirectoToHome();
+    }
+  }
+
+  onSubmit() {
+    if (!this.forgottenPasswordForm.valid) {
+      return;
+    }
+    this.authService
+      .forgottenPassword(this.forgottenPasswordForm.value)
+      .subscribe(() => (this.isEmailSent = true));
+  }
+}
