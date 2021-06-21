88bffee7123a2dac5caebfb933cc9e0d45a828b8
quentin basset <quentin.basset@querio.com>
2021-06-21 19:54:39 +0200

    :sparkles: add user interface

---
+++ src/app/interfaces/user.ts
@@ -0,0 +1,12 @@
+export interface User {
+  id: number;
+  firstName: string;
+  lastName: string;
+  popAcceuilNumber: string;
+  isVolunteer: boolean;
+  isAdmin: boolean;
+  email: string;
+  password: string;
+  createdAt: string;
+  updatedAt: string;
+}
