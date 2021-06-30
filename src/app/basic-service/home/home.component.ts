5c3f4d01e3b8bf9d342757089ef92dc5a1135d99
quentin basset <quentin.basset@querio.com>
2021-06-30 23:42:06 +0200

    :sparkles: implement table to home component

--- src/app/basic-service/home/home.component.ts
+++ src/app/basic-service/home/home.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { BasicServices } from '../../services/basic-services.service';
 import { BasicService } from '../../interfaces/basic-service';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-home',
@@ -8,12 +11,28 @@ import { BasicService } from '../../interfaces/basic-service';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  basicServices: BasicService[] = [];
+  displayedColumns: string[] = ['startAt', 'endAt', 'maxPeople', 'isClosed'];
+  dataSource!: MatTableDataSource<BasicService>;
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
   constructor(private basicServicesService: BasicServices) {}
 
   ngOnInit(): void {
-    this.basicServicesService
-      .getAll()
-      .subscribe((basicServices) => (this.basicServices = basicServices));
+    this.basicServicesService.getAll().subscribe((basicServices) => {
+      this.dataSource = new MatTableDataSource(basicServices);
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort;
+    });
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
   }
 }
