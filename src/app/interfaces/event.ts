3659ed08f691215b892d3958abc7912de48e3b66
quentin basset <quentin.basset@querio.com>
2021-07-03 15:13:35 +0200

    :sparkles: add event interface

---
+++ src/app/interfaces/event.ts
@@ -0,0 +1,12 @@
+export interface Event {
+  id?: number;
+  startAt: string;
+  endAt: string;
+  maxPeople: number;
+  title: string;
+  comment: string;
+  isFree: boolean;
+  isClosed: boolean;
+  createdAt?: string;
+  updatedAt?: string;
+}
