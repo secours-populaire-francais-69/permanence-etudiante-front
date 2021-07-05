9c8d2a62983f9df3924f3cff3f47a25fc52296ea
quentin basset <quentin.basset@querio.com>
2021-07-05 18:59:19 +0200

    :sparkles: add subscribe to show component

--- src/app/basic-service/show/show.component.ts
+++ src/app/basic-service/show/show.component.ts
@@ -3,6 +3,8 @@ import { ActivatedRoute } from '@angular/router';
 import { BasicServices } from '@services/basic-services.service';
 import { BasicService } from '@interfaces/basic-service';
 import { Observable } from 'rxjs';
+import { ToastrService } from 'ngx-toastr';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-show',
@@ -14,11 +16,23 @@ export class ShowComponent implements OnInit {
 
   constructor(
     private basicServicesService: BasicServices,
+    private toastr: ToastrService,
     private route: ActivatedRoute
   ) {}
 
   ngOnInit(): void {
     const basicServiceId = this.route.snapshot.params.basicServiceId;
     this.basicService$ = this.basicServicesService.find(basicServiceId);
+  }
+
+  subscribe(basicService: BasicService) {
+    this.basicServicesService
+      .subscribe(basicService.id)
+      .subscribe((test: any) => {
+        const startAt = moment(basicService.startAt).format('DD/MM/YYYY');
+        this.toastr.success(
+          `Tu es maintenant inscrit pour la permanence du ${startAt}!`
+        );
+      });
   }
 }
