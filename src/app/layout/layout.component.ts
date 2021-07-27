48215bf11cfe9f600a2d789e2fa7f9b01be17a12
Florian Senat <florian.senat@optisantis.com>
2021-07-27 08:53:39 +0200

    chore: run prettier

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -5,7 +5,6 @@ import { AuthService } from '@services/auth.service';
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
-  styleUrls: ['./layout.component.scss'],
 })
 export class LayoutComponent implements OnInit {
   user: User | null = null;
