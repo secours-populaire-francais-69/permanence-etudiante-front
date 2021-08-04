ec1c65470231d4c6afcf2ccb33e487d53a7f81eb
Florian Senat <florian.senat@optisantis.com>
2021-08-04 09:12:03 +0200

    feat: add #first-letter pipe

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,10 +4,21 @@ import { CommonModule } from '@angular/common';
 import { CanAccessComponent } from './components';
 import { ArrowReturnIcon } from './icons';
 import { EmailSentIllustration } from './illustrations';
+import { FirstLetterPipe } from './pipes';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [CanAccessComponent, ArrowReturnIcon, EmailSentIllustration],
-  exports: [CanAccessComponent, ArrowReturnIcon, EmailSentIllustration],
+  declarations: [
+    FirstLetterPipe,
+    CanAccessComponent,
+    ArrowReturnIcon,
+    EmailSentIllustration,
+  ],
+  exports: [
+    FirstLetterPipe,
+    CanAccessComponent,
+    ArrowReturnIcon,
+    EmailSentIllustration,
+  ],
 })
 export class SharedModule {}
