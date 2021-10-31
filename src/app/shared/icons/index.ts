6849f1d1dc51f5c4a04aa946e7d2205fa39cc6da
Florian Senat <florian.senat@optisantis.com>
2021-10-31 20:25:08 +0100

    refactor: post detail page

--- src/app/shared/icons/index.ts
+++ src/app/shared/icons/index.ts
@@ -1,3 +1,5 @@
 export * from './arrow-return.component';
 export * from './check.component';
+export * from './lock.component';
+export * from './no-lock.component';
 export * from './trash.component';
