bca855f94fa7224d82a3f0b90d76301cc6011c68
Florian Senat <florian.senat@optisantis.com>
2021-10-31 09:14:51 +0100

    feat: add textarea component

--- src/app/shared/components/index.ts
+++ src/app/shared/components/index.ts
@@ -2,3 +2,4 @@ export * from './breadcrumbs/breadcrumbs.component';
 export * from './can-access/can-access.component';
 export * from './form-group/form-group.component';
 export * from './input/input.component';
+export * from './textarea/textarea.component';
