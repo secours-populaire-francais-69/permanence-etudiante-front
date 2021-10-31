ad9562300cfa084f762f7135c424683b57ee2404
Florian Senat <florian.senat@optisantis.com>
2021-10-31 16:20:01 +0100

    refactor: page posts-list

--- src/app/post/list/list.component.ts
+++ src/app/post/list/list.component.ts
@@ -1,43 +1,19 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { PostService } from '@services/post.service';
 import { Post } from '@interfaces/post';
-import { MatPaginator } from '@angular/material/paginator';
-import { MatSort } from '@angular/material/sort';
-import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'spf-posts-list',
   templateUrl: './list.component.html',
-  styleUrls: ['./list.component.scss'],
 })
 export class ListComponent implements OnInit {
-  displayedColumns: string[] = [
-    'title',
-    'content',
-    'isForVolunteers',
-    'createdAt',
-  ];
-  dataSource!: MatTableDataSource<Post>;
-
-  @ViewChild(MatPaginator) paginator!: MatPaginator;
-  @ViewChild(MatSort) sort!: MatSort;
+  posts?: Post[];
 
   constructor(private postService: PostService) {}
 
   ngOnInit() {
     this.postService.getAll().subscribe((posts) => {
-      this.dataSource = new MatTableDataSource(posts);
-      this.dataSource.paginator = this.paginator;
-      this.dataSource.sort = this.sort;
+      this.posts = posts;
     });
-  }
-
-  applyFilter(event: Event) {
-    const filterValue = (event.target as HTMLInputElement).value;
-    this.dataSource.filter = filterValue.trim().toLowerCase();
-
-    if (this.dataSource.paginator) {
-      this.dataSource.paginator.firstPage();
-    }
   }
 }
