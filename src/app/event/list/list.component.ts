95b751e653a8b03165e1333e180fe2b9f92c1c92
Florian Senat <florian.senat@optisantis.com>
2021-11-06 16:58:25 +0100

    refactor: event list component
    still need to finish event-card

--- src/app/event/list/list.component.ts
+++ src/app/event/list/list.component.ts
@@ -1,45 +1,25 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { EventService } from '@services/event.service';
-import { Event as IEvent } from '@interfaces/event';
-import { MatPaginator } from '@angular/material/paginator';
-import { MatSort } from '@angular/material/sort';
-import { MatTableDataSource } from '@angular/material/table';
+import { Event } from '@interfaces/event';
+import { ApiStatus } from '@interfaces/api';
 
 @Component({
   selector: 'spf-event-list',
   templateUrl: './list.component.html',
-  styleUrls: ['./list.component.scss'],
 })
 export class ListComponent implements OnInit {
-  displayedColumns: string[] = [
-    'startAt',
-    'endAt',
-    'maxPeople',
-    'isClosed',
-    'title',
-    'isFree',
-  ];
-  dataSource!: MatTableDataSource<IEvent>;
-
-  @ViewChild(MatPaginator) paginator!: MatPaginator;
-  @ViewChild(MatSort) sort!: MatSort;
+  events?: Event[];
+  apiStatus: ApiStatus = 'idle';
 
   constructor(private eventService: EventService) {}
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.apiStatus = 'loading';
     this.eventService.getAll().subscribe((events) => {
-      this.dataSource = new MatTableDataSource(events);
-      this.dataSource.paginator = this.paginator;
-      this.dataSource.sort = this.sort;
+      if (!events) this.apiStatus = 'empty';
+
+      this.events = events.reverse();
+      this.apiStatus = 'success';
     });
-  }
-
-  applyFilter(event: Event) {
-    const filterValue = (event.target as HTMLInputElement).value;
-    this.dataSource.filter = filterValue.trim().toLowerCase();
-
-    if (this.dataSource.paginator) {
-      this.dataSource.paginator.firstPage();
-    }
   }
 }
